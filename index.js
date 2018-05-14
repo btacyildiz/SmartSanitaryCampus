e67e20c0a520fe7af696a1ede2691291bdf271d6
Berkay <>
2018-05-14 16:42:00 +0300

    default ui

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+
 // [START hello_world]
 // Say hello!
 app.get('/measurement', (req, res) => {
